8518974e89e759a0aabc8ba67e6098305fe011b0
kimberly ong <e0424807@u.nus.edu>
2021-09-04 21:15:05 +0800

    refactor: refactor icons logic and improve styling

--- ProfileCard.js
+++ ProfileCard.js
@@ -1,65 +1,74 @@
-import * as React from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Foundation } from '@expo/vector-icons'
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import PersonDetails from './PersonDetails';
+import * as React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Foundation } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import PersonDetails from "./PersonDetails";
 
+const ISSUE_ICONS = {
+  family: <MaterialIcons name="family-restroom" size={24} color="black" />,
+  relationship: <FontAwesome5 name="heart-broken" size={24} color="black" />,
+  studies: <Feather name="book-open" size={24} color="black" />,
+};
+
+const GENDER_ICONS = {
+  male: <Foundation name="male-symbol" size={24} color="black" />,
+  female: <Foundation name="female-symbol" size={24} color="black" />,
+};
 
 function ProfileCard({ profileDetails, navigation }) {
-
-
   function onPress() {
     navigation.navigate("Details", {
       name: profileDetails.name,
       age: profileDetails.age,
       gender: profileDetails.gender,
-      issue: profileDetails.issue
-    })
+      issue: profileDetails.issue,
+    });
   }
-
 
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.profileWhole}>
         <View style={styles.profileTop}>
-          <Text>{profileDetails.name}</Text>
-          <Text>{profileDetails.age}</Text>
+          <Text>Username: {profileDetails.name}</Text>
+          <Text>Age: {profileDetails.age}</Text>
         </View>
         <View>
+          <Text>{profileDetails.issue}</Text>
+          {ISSUE_ICONS[profileDetails.issue]}
         </View>
-          <Text>
-            {profileDetails.issue}
-          </Text>
-          {profileDetails.issue === "family" ? <MaterialIcons name="family-restroom" size={24} color="black" /> 
-                : profileDetails.issue === "relationship" ? <FontAwesome5 name="heart-broken" size={24} color="black" /> 
-                : <Feather name="book-open" size={24} color="black" /> }
-
-          <Text>{profileDetails.gender === "male" ? <Foundation name="male-symbol" size={24} color="black" /> : <Foundation name="female-symbol" size={24} color="black" />}</Text>
-
+        <View>
+          <Text>{GENDER_ICONS[profileDetails.gender]}</Text>
+        </View>
       </View>
     </TouchableOpacity>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   profileTop: {
+    display: "flex",
     flexDirection: "row",
-    width: '100%'
   },
-  profileBottom: {
-
-  }, 
   profileWhole: {
-    borderRadius: 3,
+    display: "flex",
+    flexDirection: "column",
+    marginHorizontal: 20,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 15,
     borderWidth: 2,
-    flex: 1,
-    backgroundColor: '#fdd7e4',
-  }
-})
+    backgroundColor: "#fdd7e4",
+  },
+});
 
-
-export default ProfileCard
+export default ProfileCard;
